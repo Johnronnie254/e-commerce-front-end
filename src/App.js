e62c829684b065f60f1ec2ddcc12648421b24040
Ochieng John Ronnie <johnronnie18@gmail.com>
2024-01-28 17:27:47 +0300

    productlist

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+//import ProductList from './components/ProductList'
+import ProductList from './components/new'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+     
+    <ProductList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/productList.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://online-market-msht.onrender.com/products')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+      .catch(error => console.error('Error fetching products:', error));
+  }, []); 
+
+  return (
+    <div className="container">
+      <div className="row">
+        <h2>Products</h2>
+        {products.map(product => (
+          <div key={product.id} className="col-md-4">
+            <div className="card">
+              <img src={product.imageUrl} className="card-img-top" alt={`Product ${product.id}`} />
+              <div className="card-body">
+                <h5 className="card-title">{product.name}</h5>
+                <p className="card-text">{product.image_url}</p>
+                <p className="card-text">Price: ${product.price}</p>
+                <a href="#" className="btn btn-primary">Add to Cart</a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
